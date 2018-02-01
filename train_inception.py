4e5cdf755d25ff6523293473a88885f1f4e94263
Yichen Qiu <yichen.qiu@gmail.com>
2018-01-31 18:22:16 -0800

    updated the best performing model

--- train_inception.py
+++ train_inception.py
@@ -6,11 +6,11 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 from keras import backend as K
 from keras.models import Sequential, Model
-from keras.callbacks import EarlyStopping
+#from keras.callbacks import EarlyStopping
 import numpy as np
-import pickle as pk
-from keras import regularizers
-from sklearn.metrics import accuracy_score,log_loss
+#import pickle as pk
+#from keras import regularizers
+#from sklearn.metrics import accuracy_score,log_loss
 
 def feature_extraction_InV3(img_width, img_height,
                         train_data_dir,
@@ -39,7 +39,7 @@ def feature_extraction_InV3(img_width, img_height,
     print (X_train.shape,y_train1.shape)
     return X_train,y_train1,model
 
-def train_last_layer(test_data_dir,img_width, img_height,
+def train_last_layer(img_width, img_height,
                         train_data_dir,
                         num_image,
                         epochs = 50):
@@ -49,35 +49,20 @@ def train_last_layer(test_data_dir,img_width, img_height,
                             epochs)
     my_model = Sequential()
     my_model.add(BatchNormalization(input_shape=X_train.shape[1:]))
-    my_model.add(Dense(256, activation = "relu"))
+    my_model.add(Dense(1024, activation = "relu"))
     my_model.add(Dense(4, activation='softmax'))
     my_model.compile(optimizer="SGD", loss='categorical_crossentropy',metrics=['accuracy'])
     #early = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1, mode='auto')
-    my_model.fit(X_train, y_train,epochs=100,batch_size=30,validation_split=0.1,verbose=1)
-    test_generator=ImageDataGenerator(rescale=1. / 255).flow_from_directory(test_data_dir,
-    target_size = (299, 299),
-    batch_size = 15,
-    class_mode = "categorical",
-    shuffle=False)
-    y_test=test_generator.classes
-    test_generator.reset
-    X_test=model.predict_generator(test_generator,verbose=1)
-    y_pred=my_model.predict(X_test)
-    y_pred_prob=my_model.predict_proba(X_test)
-    accuracy=accuracy_score(y_test,y_pred.argmax(axis=1))
-    loss=log_loss(y_test,y_pred_prob)
-    print ("Accuracy score: {}".format (accuracy))
-    print ("Log_loss score: {}".format (loss))
+    my_model.fit(X_train, y_train,epochs=18,batch_size=30,verbose=1)
     return my_model
 
 if __name__=="__main__":
     img_width=299
     img_height = 299
-    train_data_dir = "data/train"
-    test_data_dir="data/test"
-    num_image=1439
+    train_data_dir = "all_image_final"
+    num_image=1800
     epochs = 10
-    model=train_last_layer(test_data_dir,img_width, img_height,
+    model=train_last_layer(img_width, img_height,
                             train_data_dir,
                             num_image,epochs)
 
